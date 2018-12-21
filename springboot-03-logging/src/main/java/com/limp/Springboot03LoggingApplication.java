cbcec77f843b4b49a8c8cdc0eb5ca91ee1f575ba
十年呵护 <907820813@qq.com>
2018-12-21 09:28:41 +0800

    SpringBoot日志模块初步版本提交

---
+++ springboot-03-logging/src/main/java/com/limp/Springboot03LoggingApplication.java
@@ -0,0 +1,14 @@
+package com.limp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot03LoggingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springboot03LoggingApplication.class, args);
+	}
+
+}
+
