ba066fd6a7cf24ea34e4f7a06bc3dd706cda75cd
十年呵护 <907820813@qq.com>
2018-12-23 22:19:36 +0800

    SpringBoot  05 模块提交

---
+++ springboot-05-test-starter/src/main/java/com/limp/Springboot05TestStarterApplication.java
@@ -0,0 +1,15 @@
+package com.limp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot05TestStarterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springboot05TestStarterApplication.class, args);
+		System.out.println("启动成功...");
+	}
+
+}
+
