b32ca45aa53e0120d98bf14eb4f6a1ed5b3f2910
十年呵护 <907820813@qq.com>
2018-12-25 16:05:44 +0800

    SpringBoot

---
+++ springboot-06-jpa-transactional/src/main/java/com/limp/Springboot06JpaTransactionalApplication.java
@@ -0,0 +1,14 @@
+package com.limp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot06JpaTransactionalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springboot06JpaTransactionalApplication.class, args);
+	}
+
+}
+
---
+++ springboot-06-jpa-transactional/src/test/java/com/limp/springboot06jpatransactional/Springboot06JpaTransactionalApplicationTests.java
@@ -0,0 +1,17 @@
+package com.limp.springboot06jpatransactional;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Springboot06JpaTransactionalApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
